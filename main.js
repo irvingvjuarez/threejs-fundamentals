6b9385a24a44f8f2d03010ffd17260db8e0753cf
Irving Juárez <irvingjuarez274@gmail.com>
2025-02-24 00:01:59 -0600

    validation for webgl2 added

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
-import * as THREE from 'three';
-import { ASPECT, FAR_NUMBER, FIELD_OF_VIEW, NEAR_NUMBER } from './globals/constants';
-import { animate } from './utils/render';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { setup } from './utils/setup';
 // import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // const controls = new OrbitControls( camera, renderer.domElement );
 // const loader = new GLTFLoader();
 
-// Creating a scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    FIELD_OF_VIEW,
-    ASPECT, 
-    NEAR_NUMBER,
-    FAR_NUMBER
-);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Adding a cube to the scene
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-
-scene.add(cube);
-camera.position.z = 5;
-
-// Rendering the whole scene
-animate(renderer, () => {
-    renderer.render(scene, camera);
-    
-    // Rotating the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-});
+// Verifying the web browser accepts WebGL
+if (WebGL.isWebGL2Available()) {
+    setup();
+} else {
+    const warning = WebGL.getWebGL2ErrorMessage();
+	document.body.appendChild( warning );
+}
---
+++ utils/setup.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+import { ASPECT, FAR_NUMBER, FIELD_OF_VIEW, NEAR_NUMBER } from '../globals/constants';
+import { animate } from './render';
+
+export function setup() {
+    // Creating a scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+        FIELD_OF_VIEW,
+        ASPECT, 
+        NEAR_NUMBER,
+        FAR_NUMBER
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    // Adding a cube to the scene
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    
+    scene.add(cube);
+    camera.position.z = 5;
+    
+    // Rendering the whole scene
+    animate(renderer, () => {
+        renderer.render(scene, camera);
+        
+        // Rotating the cube
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    });
+}
