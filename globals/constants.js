535e72b1a330539f3df7d65c20d6a8465121304d
Irving Juárez <irvingjuarez274@gmail.com>
2025-02-24 02:22:58 -0600

    Creating a line and making it rotate

--- globals/constants.js
+++ globals/constants.js
@@ -1,4 +1,4 @@
-export const FIELD_OF_VIEW = 75;
+export const FIELD_OF_VIEW = 45;
 export const ASPECT = window.innerWidth / window.innerHeight;
-export const NEAR_NUMBER = 0.1;
-export const FAR_NUMBER = 1000;
+export const NEAR_NUMBER = 1;
+export const FAR_NUMBER = 1000 / 2;
--- utils/setup.js
+++ utils/setup.js
@@ -16,12 +16,25 @@ export function setup() {
     document.body.appendChild(renderer.domElement);
     
     // Adding a cube to the scene
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
+    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     
     scene.add(cube);
-    camera.position.z = 5;
+    camera.position.set(0,0,100);
+    camera.lookAt(0,0,0)
+
+    // Adding a line to the scene
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    const linePoints = [
+        new THREE.Vector3(-10, 10, 0),
+        new THREE.Vector3(0, 10, 0),
+        new THREE.Vector3(10, 10, 0),
+    ];
+    const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
+    const line = new THREE.Line(lineGeometry, lineMaterial);
+
+    scene.add(line);
     
     // Rendering the whole scene
     animate(renderer, () => {
@@ -30,5 +43,8 @@ export function setup() {
         // Rotating the cube
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
+
+        // Trying to rotate the line
+        line.rotation.y += 0.01;
     });
 }
