454ae68026b5ddcd8a160e33d9d3fae44aa9174b
Irving Juárez <irvingjuarez274@gmail.com>
2025-03-02 12:31:16 -0600

    Refactoring

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 import WebGL from 'three/addons/capabilities/WebGL.js';
-import { setup } from './utils/setup';
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
-// const controls = new OrbitControls( camera, renderer.domElement );
-// const loader = new GLTFLoader();
+import { setup } from './setup';
+import { populatePrimitives } from './setup/populatePrimitives';
 
 // Verifying the web browser accepts WebGL
 if (WebGL.isWebGL2Available()) {
-    setup();
+    const renderRequested = setup();
+    populatePrimitives(renderRequested);
 } else {
     const warning = WebGL.getWebGL2ErrorMessage();
 	document.body.appendChild( warning );
