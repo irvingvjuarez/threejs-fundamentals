d2f8b8f111be256313b12a49eb010c5236f9bd8e
Irving Juárez <irvingjuarez274@gmail.com>
2025-03-08 22:07:03 -0600

    GEOMETRIES list as a constant

--- globals/constants.js
+++ globals/constants.js
@@ -1,4 +1,31 @@
+import { makeInstance } from "../utils/makeInstance";
+import { render, requestRenderIfNotRequested } from "../utils/render";
+import { scene } from "../utils/scene";
+
 export const FIELD_OF_VIEW = 45;
 export const ASPECT = window.innerWidth / window.innerHeight;
 export const NEAR_NUMBER = 1;
 export const FAR_NUMBER = 1000 / 2;
+
+export const GEOMETRIES = [
+    {
+        name: 'Cube',
+        instance: null,
+        setInstance(renderRequested) {
+            this.instance = makeInstance({
+                handlerChange: () => requestRenderIfNotRequested(renderRequested),
+                instanceName: 'Cube', 
+                color: 0x44aa88, 
+                x: 0
+            });
+            return this;
+        },
+        getInstance() {
+            return this.instance
+        },
+        handler(renderRequested) {
+            scene.add(this.instance);
+            render(renderRequested);
+        }
+    }
+];
--- setup/populatePrimitives.js
+++ setup/populatePrimitives.js
@@ -1,35 +1,11 @@
+import { GEOMETRIES } from '../globals/constants';
 import { gui } from '../utils/gui';
-import { makeInstance } from '../utils/makeInstance';
-import { render, requestRenderIfNotRequested } from '../utils/render';
-import { scene } from '../utils/scene';
-
-function getPrimitives(renderRequested) {
-    return [
-        {
-            name: 'Cube',
-            handler: () => {
-                const instance = makeInstance({
-                    handlerChange: () => requestRenderIfNotRequested(renderRequested),
-                    instanceName: 'Cube', 
-                    color: 0x44aa88, 
-                    x: 0
-                });
-                scene.add(instance);
-                render(renderRequested);
-
-                return instance;
-            }
-        }
-    ];
-}
-
-function renderPrimitives(primitives, renderRequested) {
-    const [geometries] = gui.controllers;
-    const currentGeometry = geometries.initialValue;
-    const currentInstance = primitives.find(primitive => primitive.name === currentGeometry)?.handler();
-}
 
 export function populatePrimitives(renderRequested) {
-    const primitives = getPrimitives(renderRequested);
-    renderPrimitives(primitives, renderRequested);
+    const primitives = GEOMETRIES;
+    const [geometries] = gui.controllers;
+    const currentGeometryName = geometries.initialValue;
+    const currentGeometryPrimitive = primitives.find(primitive => primitive.name === currentGeometryName);
+    const currentInstance = currentGeometryPrimitive.setInstance(renderRequested);
+    currentInstance.handler(renderRequested);
 }
